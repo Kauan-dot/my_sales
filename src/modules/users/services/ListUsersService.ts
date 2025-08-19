4f523bf55d1eacb2557c876e0efcfe9f3d26b872
Kauan-dot <kauanaffonso115@gmail.com>
2025-08-19 00:20:41 -0300

    feat: list user service and user controller

---
+++ src/modules/users/services/ListUsersService.ts
@@ -0,0 +1,10 @@
+import { User } from "../database/entities/User";
+import { usersRepositories } from "../database/repositories/UsersRepositories";
+
+export default class ListUsersService {
+    async execute(): Promise<User[]> {
+        const users = await usersRepositories.find();
+        return users;
+    }
+}
+    
